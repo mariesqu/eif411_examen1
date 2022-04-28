b2873eb0696db781999c20c340a57160330bb1d9
Mario Esquivel <mesquivel007@gmail.com>
2022-04-27 20:54:00 -0400

    Create Paciente.java
    section #1

---
+++ src/entities/Paciente.java
@@ -0,0 +1,31 @@
+package entities;
+
+public final class Paciente {
+	private final String nombre;
+	private final Integer edad;
+	private final Integer temperatura;
+	private final Boolean vomito;
+
+	Paciente(String nombre, Integer edad, Integer temperatura, Boolean vomito) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.temperatura = temperatura;
+		this.vomito = vomito;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public Integer getTemperatura() {
+		return temperatura;
+	}
+
+	public Boolean getVomito() {
+		return vomito;
+	}
+}
